3b33f98320f4d1a03f73b8bbb95edc12b5670f34
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-04-22 17:42:50 +0800

    edited verify route function

--- routes/auditRoute.js
+++ routes/auditRoute.js
@@ -7,14 +7,14 @@ const verify = require('../middleware/verifyAuth').verifyStaff
 
 
 //POST functions
-router.post('/createaudit',verify,audit.createAudit)
-router.get('/seenoncomp',verify,audit.getNonCompliance)
-router.get('/ongoingaudits',verify,audit.ViewuncompletedAudits)
-router.get('/pastaudits',verify,audit.pastAudits)
-router.post('/resolveAudits',verify,audit.resolveAudits)
-router.delete('/deleteResAud',verify,audit.delNewAudits)
-router.get('/viewtenantDeets',verify,audit.getTenantDeets)
-router.put('/update',verify,audit.updateAudit)
+router.post('/createaudit',verify.verifyStaff,audit.createAudit)
+router.get('/seenoncomp',verify.verifyStaff,audit.getNonCompliance)
+router.get('/ongoingaudits',verify.verifyStaff,audit.ViewuncompletedAudits)
+router.get('/pastaudits',verify.verifyStaff,audit.pastAudits)
+router.post('/resolveAudits',verify.verifyStaff,audit.resolveAudits)
+router.delete('/deleteResAud',verify.verifyStaff,audit.delNewAudits)
+router.get('/viewtenantDeets',verify.verifyStaff,audit.getTenantDeets)
+router.put('/update',verify.verifyStaff,audit.updateAudit)
 
 
 module.exports = router

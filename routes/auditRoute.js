670f54836a4d68b2cf6d77f0550a27d938299ee9
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-04-22 17:43:07 +0800

    edited route

--- routes/auditRoute.js
+++ routes/auditRoute.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 //import the staff verification
-const verify = require('../middleware/verifyAuth').verifyStaff
+const verify = require('../middleware/verifyAuth')
 
 
 //POST functions
